fb70d60ba5bc15fb0234f21b7253350c82b6f9ea
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2025-05-10 14:05:29 -0300

    adicionei o botão minha localização

--- app/src/main/java/com/example/pratica3_maps/MainActivity.java
+++ app/src/main/java/com/example/pratica3_maps/MainActivity.java
@@ -3,17 +3,17 @@ package com.example.pratica3_maps;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends ListActivity {
 
-    public LatLng ITAOCARA = new LatLng(-21.667090042336575, -42.081350019614895);
-    public LatLng VICOSA = new LatLng(-20.756845864540256, -42.87517877659752);
+    public LatLng ITAOCARA = new LatLng(-21.674786599501207, -42.087803755987444);
+    public LatLng VICOSA = new LatLng(-20.756855651371684, -42.8751327472389);
     public LatLng DPI = new LatLng(-20.764807038105072, -42.86837497388309);
 
     @Override
@@ -21,7 +21,7 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
 
         String[] menu = new String [] {"Minha casa na cidade natal", "Minha casa em Viçosa", "Meu departamento", "Fechar aplicação"};
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menu);
         setListAdapter(arrayAdapter);
     }
 
@@ -29,24 +29,29 @@ public class MainActivity extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
 
         Intent it = new Intent(getBaseContext(), MostrarMapa.class);
+        it.putExtra("lat1", ITAOCARA.latitude);
+        it.putExtra("lng1", ITAOCARA.longitude);
+        it.putExtra("lat2", VICOSA.latitude);
+        it.putExtra("lng2", VICOSA.longitude);
+        it.putExtra("lat3", DPI.latitude);
+        it.putExtra("lng3", DPI.longitude);
+
+        String aux = l.getItemAtPosition(position).toString();
 
         switch(position) {
             case 0:
-                it.putExtra("lat", ITAOCARA.latitude);
-                it.putExtra("lng", ITAOCARA.longitude);
-                Log.d("Debug", "Opção itaocara");
+                Toast.makeText(getBaseContext(), aux, Toast.LENGTH_SHORT).show();
+                it.putExtra("local", "itaocara");
                 startActivity(it);
                 break;
             case 1:
-                it.putExtra("lat", VICOSA.latitude);
-                it.putExtra("lng", VICOSA.longitude);
-                Log.d("Debug", "Opção viçosa");
+                Toast.makeText(getBaseContext(), aux, Toast.LENGTH_SHORT).show();
+                it.putExtra("local", "vicosa");
                 startActivity(it);
                 break;
             case 2:
-                it.putExtra("lat", DPI.latitude);
-                it.putExtra("lng", DPI.longitude);
-                Log.d("Debug", "Opção dpi");
+                Toast.makeText(getBaseContext(), aux, Toast.LENGTH_SHORT).show();
+                it.putExtra("local", "dpi");
                 startActivity(it);
                 break;
             case 3:
--- app/src/main/java/com/example/pratica3_maps/MostrarMapa.java
+++ app/src/main/java/com/example/pratica3_maps/MostrarMapa.java
@@ -1,24 +1,34 @@
 package com.example.pratica3_maps;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.Manifest;
+import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.DecimalFormat;
+
 public class MostrarMapa extends FragmentActivity implements OnMapReadyCallback {
-    public LatLng ITAOCARA = new LatLng(-21.667090042336575, -42.081350019614895);
-    public LatLng VICOSA = new LatLng(-20.756845864540256, -42.87517877659752);
-    public LatLng DPI = new LatLng(-20.764807038105072, -42.86837497388309);
-    public LatLng coordenada;
+    public LatLng ITAOCARA, VICOSA, DPI, coordenada;
+    private FusedLocationProviderClient minhaLocalizacao;
+    public Marker meuMarcador;
     private GoogleMap map;
 
     @Override
@@ -27,36 +37,80 @@ public class MostrarMapa extends FragmentActivity implements OnMapReadyCallback 
         setContentView(R.layout.activity_mostrar_mapa);
 
         Intent it = getIntent();
-        double lat = it.getDoubleExtra("lat", 0);
-        double lng = it.getDoubleExtra("lng", 0);
-        coordenada = new LatLng(lat, lng);
+        ITAOCARA = new LatLng(it.getDoubleExtra("lat1", 0), it.getDoubleExtra("lng1", 0));
+        VICOSA = new LatLng(it.getDoubleExtra("lat2", 0), it.getDoubleExtra("lng2", 0));
+        DPI = new LatLng(it.getDoubleExtra("lat3", 0), it.getDoubleExtra("lng3", 0));
+        String local = it.getStringExtra("local");
+
+        if("itaocara".equals(local)){
+            coordenada = ITAOCARA;
+        } else if ("vicosa".equals(local)) {
+            coordenada = VICOSA;
+        } else {
+            coordenada = DPI;
+        }
+
         ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa)).getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        map.addMarker(new MarkerOptions().position(ITAOCARA).title("Cidade natal"));
+        map.addMarker(new MarkerOptions().position(ITAOCARA).title("Minha casa em Itaocara"));
         map.addMarker(new MarkerOptions().position(VICOSA).title("Minha casa em Viçosa"));
-        map.addMarker(new MarkerOptions().position(DPI).title("DPI"));
+        map.addMarker(new MarkerOptions().position(DPI).title("DPI/UFV"));
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(coordenada, 16));
     }
 
-    public void onClickItaocara(View v) {
+    public void Itaocara(View v) {
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ITAOCARA, 16);
         map.animateCamera(update);
     }
 
-    public void onClickVicosa(View v) {
+    public void Vicosa(View v) {
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(VICOSA, 16);
         map.animateCamera(update);
     }
 
-    public void onClickDPI(View v) {
+    public void DPI(View v) {
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(DPI, 16);
         map.animateCamera(update);
+    }
+
+    public void minhaLocalizacao(View v) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+
+        minhaLocalizacao = LocationServices.getFusedLocationProviderClient(this);
+        minhaLocalizacao.getLastLocation().addOnSuccessListener(location -> {
+            if (location != null) {
+                LatLng ATUAL = new LatLng(location.getLatitude(), location.getLongitude());
+
+                Location posicaoAtual = new Location("");
+                posicaoAtual.setLatitude(location.getLatitude());
+                posicaoAtual.setLongitude(location.getLongitude());
+
+                Location vicosa = new Location("");
+                vicosa.setLatitude(VICOSA.latitude);
+                vicosa.setLongitude(VICOSA.longitude);
+
+                double distancia = posicaoAtual.distanceTo(vicosa);
+                DecimalFormat df = new DecimalFormat("0.##");
+                String distanciaString = df.format(distancia);
+                Toast.makeText(getBaseContext(), distanciaString +" m", Toast.LENGTH_SHORT).show();
+
+                if(meuMarcador != null) {
+                    meuMarcador.remove();
+                }
+
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(ATUAL, 16));
+                meuMarcador = map.addMarker(new MarkerOptions().position(ATUAL).title("Minha localização atual").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+            }
+        });
     }
 }
